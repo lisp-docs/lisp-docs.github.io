edbce1684f9b9504a200de0142508e0d6288a516
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-12 12:53:39 +0200

    Docusaurus Config title, tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Lisp Docs',
+  tagline: 'The (Un)Official LISP Documentation ',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
