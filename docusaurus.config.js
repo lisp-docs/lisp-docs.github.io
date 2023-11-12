0793b7ac49f4528ae3b520f901e235ac8177f02e
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-12 17:49:15 +0200

    added lisp code coloring in codeblocks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,6 +136,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ['powershell', 'python',  'lisp',],
       },
     }),
 };
